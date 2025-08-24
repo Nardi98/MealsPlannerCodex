18dd40940f16f8592c38a630941146200cd468aa
Nardi98 <64101118+Nardi98@users.noreply.github.com>
2025-08-24 10:37:29 +0200

    fix docker-compose services

--- backend/main.py
+++ backend/main.py
@@ -59,4 +59,4 @@ def set_plan(payload: schemas.MealPlanCreate, db: Session = Depends(get_db)) -> 
 if __name__ == "__main__":
     import uvicorn
 
-    uvicorn.run("backend.main:app", host="0.0.0.0", port=8000)
+    uvicorn.run("main:app", host="0.0.0.0", port=8000)
