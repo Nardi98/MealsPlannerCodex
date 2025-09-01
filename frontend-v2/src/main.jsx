40f249bffde023568f1defa5ed2afdb8e73ea0be
Nardi98 <64101118+Nardi98@users.noreply.github.com>
2025-09-01 23:33:39 +0200

    feat: add design tokens hook

--- frontend-v2/src/main.jsx
+++ frontend-v2/src/main.jsx
@@ -2,9 +2,15 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { useCssVars } from './tokens.js'
+
+// eslint-disable-next-line react-hooks/rules-of-hooks
+const cssVars = useCssVars()
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <div style={cssVars}>
+      <App />
+    </div>
   </StrictMode>,
 )
